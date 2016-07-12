dfd88ac314a04d51c8fd1c9cbd3063629ffced16
nima salehi vaio Darvish mirdamad <me@nimasalehi.com>
2016-07-12 14:14:35 +0430

    version

---
+++ ns-portfolio-post-type.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Plugin Name: Nima Salehi Portfolio Post Type
+ * Plugin URI: http://www.nimasalehi.com/ns-portfolio-post-type
+ * Description: Portfolio Post Type
+ * Version: 0.5
+ * Author: Nima Salehi
+ * Author URI: nimasalehi.com
+*/
+
+###############################################################################
+// Version of the plugin
+global $ns_ppt_version;
+$ns_ppt_version='0.5';
